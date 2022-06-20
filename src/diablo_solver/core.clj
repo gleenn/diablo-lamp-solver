037910f4c6e5f6b7b65f675ebb2e2544922e9deb
Glenn Jahnke <gleenn@apple.com>
2022-06-19 20:57:37 -0700

    everything but actual solve working

--- src/diablo_solver/core.clj
+++ src/diablo_solver/core.clj
@@ -1,6 +1,37 @@
 (ns diablo-solver.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn solved? [state]
+  (every? true? state))
+
+(def transitions
+  {0 [0 1 3]
+   1 [1 0 2 4]
+   2 [2 1 5]
+   3 [3 0 4 6]
+   4 [4 1 3 5 7]
+   5 [5 2 4 8]
+   6 [6 3 7]
+   7 [7 4 6 8]
+   8 [8 5 7]})
+
+(defn apply-transition [state transition]
+  (-> state
+      (update :state #(reduce (fn [state toggle-index]
+                                (update state toggle-index not))
+                              %
+                              (transitions transition)))
+      (update :steps #(conj % transition))))
+
+(defn solve
+  ([current-state]
+   (solve [{:state current-state :steps []}] 1))
+  ([states max-depth]
+   (prn :states)
+   (doseq [state states]
+     (prn :state state))
+   (if-let [solution (first (map solved? (map :state states)))]
+     (:steps solution)
+     (if (zero? max-depth)
+       :no-solution-found
+       (solve (mapcat #(map (partial apply-transition %) transitions) states)
+              (dec max-depth))))))
--- test/diablo_solver/core_test.clj
+++ test/diablo_solver/core_test.clj
@@ -2,6 +2,50 @@
   (:require [clojure.test :refer :all]
             [diablo-solver.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest apply-transition-test
+  (is (= {:state [true true true
+                  true true true
+                  true true true]
+          :steps [8]}
+         (apply-transition
+          {:state [true true true
+                   true true false
+                   true false false]
+           :steps []}
+          8)))
+  (is (= {:state [true false true
+                  false false false
+                  true false true]
+          :steps [8 4]}
+         (apply-transition
+          {:state [true true true
+                   true true true
+                   true true true]
+           :steps [8]}
+          4)))
+  (is (= {:state [true false true
+                  false false false
+                  true false true]
+          :steps [8 4 0]}
+         (apply-transition
+          {:state [false true true
+                   true false false
+                   true false true]
+           :steps [8 4]}
+          0))))
+
+(deftest solved?-test
+  (is (not (solved? [true true true true true false true false false])))
+  (is (solved? [true true true true true true true true true])))
+
+(deftest solve-test
+  (is (= :no-solution-found
+         (solve [{:state [true true true
+                          true true false
+                          true false false]
+                  :steps []}]
+                0)))
+  (is (= [8]
+         (solve [true true true
+                 true true false
+                 true false false]))))
