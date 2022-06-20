1bc0f4f43e9f07c73dd772f11b5c4b21bfbe6e86
Glenn Jahnke <gleenn@apple.com>
2022-06-19 20:18:01 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject diablo-solver "0.1.0-SNAPSHOT"
+  :description "Simple breadth-first searcher for lamp-lighting problem"
+  :license {:name "MIT" :url ""}
+  :dependencies [[org.clojure/clojure "1.11.0"]]
+  :repl-options {:init-ns diablo-solver.core})
